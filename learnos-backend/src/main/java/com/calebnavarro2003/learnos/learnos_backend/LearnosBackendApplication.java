5f4489b111b2d55d881ce65a9f3b1bd2ba48ad21
Caleb Navarro <calebnavarro@ufl.edu>
2025-02-06 15:26:10 -0500

    setup spring boot

---
+++ learnos-backend/src/main/java/com/calebnavarro2003/learnos/learnos_backend/LearnosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.calebnavarro2003.learnos.learnos_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnosBackendApplication.class, args);
+	}
+
+}
---
+++ learnos-backend/src/test/java/com/calebnavarro2003/learnos/learnos_backend/LearnosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calebnavarro2003.learnos.learnos_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
