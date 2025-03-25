05b6476fa9345e579ecba02fb1463a5e66a05ff0
Caleb Navarro <calebnavarro@ufl.edu>
2025-03-25 12:12:10 -0400

    Stores responses for a user in responses table

--- learnos-backend/src/main/java/com/calebnavarro2003/learnos/learnos_backend/Repository/AnswerRepository.java
+++ learnos-backend/src/main/java/com/calebnavarro2003/learnos/learnos_backend/Repository/AnswerRepository.java
@@ -6,9 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.calebnavarro2003.learnos.learnos_backend.Model.Answer;
+import com.calebnavarro2003.learnos.learnos_backend.Model.QuestionResult;
 
 
 @Repository
 public interface AnswerRepository extends JpaRepository<Answer, Integer> {
     Answer findLetterByquestionId(int questionId);
+    void save(QuestionResult questionResult);
 }
