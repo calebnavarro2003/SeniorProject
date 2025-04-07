e0b7ef60fa72f0564080d23f26708f859d9814ad
aidengaul <100671205+aidengaul@users.noreply.github.com>
2025-04-07 14:28:06 -0400

    Update UserService.js

--- frontend/src/Services/UserService.js
+++ frontend/src/Services/UserService.js
@@ -1,61 +1,77 @@
 import axios from "axios";
 
-class UserService{
-    static BASE_URL = "http://localhost:8080"
+const BASE_URL = "http://localhost:8080";
 
-    static async getAllModules(){
-        try{
-            const response = await axios.get(`${UserService.BASE_URL}/module/allmodules`, {
-                withCredentials: true
-            })
-            console.log(response)
-            return response.data;
-        }catch(err){
-            throw err;
-        }
-    }
-
-    static async getModuleDetails(moduleId){
-        try{
-            const response = await axios.get(`${UserService.BASE_URL}/questions/module/${moduleId}`, {
-                withCredentials: true
-            })
-            console.log(response)
-            return response.data;
-        }catch(err){
-            throw err;
-        }
-    }
-
-    static async getUserInfo(){
-        try{
-            const response = await axios.get(`${UserService.BASE_URL}/get-user-info`, {
-                withCredentials: true
-            })
-            console.log(response)
-            return response.data;
-        }catch(err){
-            throw err;
-        }
-    }
-
-    static async submitAnswers(answers){
-        try {
-            const response = await axios.post(`${UserService.BASE_URL}/answers/grade`, answers, 
-                { 
-                    withCredentials: true, 
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                }
-            )
-            return response.data
-        } catch (error) {
-            console.error("Error submitting answers: " + error)
-            throw(error)
-        }
-    }
-
+export const fetchUserInfo = async () => {
+  try{
+    const response = await axios.get(`${BASE_URL}/get-user-info`, {
+        withCredentials: true
+      })
+    return response.data;
+  } catch(err){
+      throw err;
+  }
 }
 
-export default UserService;
+export const fetchUserGrade = async (userId, moduleId) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/answers/percentage/${userId}/${moduleId}`, { withCredentials: true });
+    return response.data.percentage;
+  } catch (error) {
+    console.error("Error fetching user's grade:", error);
+    throw error;
+  }
+};
+
+export const fetchAllModules = async () => {
+  try{
+    const response = await axios.get(`${BASE_URL}/module/allmodules`, {
+        withCredentials: true
+    })
+    console.log(response)
+    return response.data;
+  }catch(err) {
+      throw err;
+}
+}
+
+export const fetchAllModuleGrades = async (userId) => {
+    try {
+      const response = await axios.get(`${BASE_URL}/answers/grades/${userId}`, { withCredentials: true });
+      return response.data; // Assume it returns an array of { moduleId, percentage }
+    } catch (error) {
+      console.error("Error fetching all modules grades:", error);
+      throw error;
+    }
+  };
+
+export const fetchModuleAnswers = async (moduleId) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/answers/${moduleId}`, { withCredentials: true });
+    return response.data.map(({ questionId, letter }) => ({ questionId, letter }));
+  } catch (error) {
+    console.error("Error fetching answers for a given module", error);
+    throw error;
+  }
+};
+
+export const fetchModuleDetails = async (moduleId) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/questions/module/${moduleId}`, { withCredentials: true });
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching module details!", error);
+    throw error;
+  }
+};
+
+export const submitAnswers = async (answers) => {
+  try {
+    const response = await axios.post(`${BASE_URL}/answers/grade`, answers, { withCredentials: true, headers: { 'Content-Type': 'application/json' } });
+    return response.data;
+  } catch (error) {
+    console.error("Error submitting answers:", error);
+    throw error;
+  }
+};
+
