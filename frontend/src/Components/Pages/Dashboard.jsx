771e02988ef910292fdcb6434d32be838ec78074
aidengaul <aiden@gaulclan.com>
2025-04-08 09:42:46 -0400

    Add progress map to home page

--- frontend/src/Components/Pages/Dashboard.jsx
+++ frontend/src/Components/Pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { messages } from '../../constants/WelcomeMessages';
 import { fetchUserInfo, fetchAllModuleGrades } from '../../Services/UserService';
+import ProgressMap from '../ProgressMap';
 
 const getRandomMessage = (messageArray) => {
   const randomIndex = Math.floor(Math.random() * messageArray.length);
@@ -107,7 +108,7 @@ export default function Dashboard() {
       <div className="flex flex-col flex-1 bg-white rounded-lg shadow-md h-full">
         <div className="text-3xl my-8 ml-8">Your current progress</div>
         <div className="w-full h-full px-4 pb-4">
-          {/* Add progress map here */}
+          <ProgressMap />
         </div>
       </div>
     </div>
